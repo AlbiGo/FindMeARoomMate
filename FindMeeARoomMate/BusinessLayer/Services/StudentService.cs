10b64f73b84e6f5bbc09669605f5aee9979eefcc
Albi.Goxhaj <Albi.Goxhaj@fti.edu.al>
2023-09-03 11:05:56 +0200

    Added service

---
+++ FindMeeARoomMate/BusinessLayer/Services/StudentService.cs
@@ -0,0 +1,21 @@
+﻿using FindMeeARoomMate.DataLayer.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMeeARoomMate.BusinessLayer.Services
+{
+    public class StudentService
+    {
+        private readonly FindMeRoomMateDBContext _dbContext;
+
+        public StudentService()
+        {
+            _dbContext = new FindMeRoomMateDBContext();
+        }
+
+
+    }
+}
